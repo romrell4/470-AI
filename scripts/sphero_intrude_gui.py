b7ec3986d6765a56fef7959742a43396c97e478b
walter <daqing.yi@byu.edu>
2016-04-22 16:02:32 -0600

    add new gui

---
+++ scripts/sphero_intrude_gui.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+import sys, rospy, math
+from PyQt4 import QtGui, QtCore
+from geometry_msgs.msg import Twist
+from std_msgs.msg import ColorRGBA, Float32, Bool
+
+
+class SpheroIntrudeForm(QtGui.QWidget):
+    
+    def __init__(self):
+        super(QtGui.QWidget, self).__init__()
+        self.resize(600, 480) 
+        self.initUI()
+
+        rospy.init_node('sphero_intrude', anonymous=True)
+        self.cmdVelPub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.cmdVelSub = rospy.Subscriber("cmd_vel", Twist, self.cmdVelCallback)   
+        
+       
+        
+    def initUI(self):
+
+        self.stateLabel = QtGui.QLabel("Position")
+        self.stateTextbox = QtGui.QTextEdit()
+        self.stateTextbox.setReadOnly(True)     
+        
+        key_instruct_label = """
+	Control Your Sphero!
+	---------------------------
+	Moving around:
+	   u    i    o
+	   j    k    l
+	   m    ,    .
+	"""
+        self.keyInstructLabel = QtGui.QLabel(key_instruct_label)
+        self.cmdVelLabel = QtGui.QLabel("cmd_vel")
+        self.cmdVelTextbox = QtGui.QTextEdit()
+        self.cmdVelTextbox.setReadOnly(True)  
+        self.connect(self, QtCore.SIGNAL("sendCmdVelText(PyQt_PyObject)"), self.updateCmdVelTextbox)
+
+        self.aprilTagsInfoLabel = QtGui.QLabel("april tags info")
+        self.aprilTagsTextbox = QtGui.QTextEdit()
+        self.aprilTagsTextbox.setReadOnly(True)
+
+        self.layout =  QtGui.QVBoxLayout()
+        self.layout.addWidget(self.stateLabel)
+        self.layout.addWidget(self.stateTextbox)
+        self.layout.addWidget(self.keyInstructLabel)
+        self.layout.addWidget(self.cmdVelLabel)
+        self.layout.addWidget(self.cmdVelTextbox)
+        self.layout.addWidget(self.aprilTagsInfoLabel)
+        self.layout.addWidget(self.aprilTagsTextbox)
+        self.setLayout(self.layout)
+
+        self.setWindowTitle("Sphero Intrude")
+        self.show()
+
+    def keyPressEvent(self, e): 
+        twist = None       
+        if e.key() == QtCore.Qt.Key_U:
+            twist = Twist()
+            twist.linear.x = -20; twist.linear.y = 20; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_I:
+            twist = Twist()  
+            twist.linear.x = 0; twist.linear.y = 20; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0     
+        elif e.key() == QtCore.Qt.Key_O:
+            twist = Twist()
+            twist.linear.x = 20; twist.linear.y = 20; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_J:
+            twist = Twist()
+            twist.linear.x = -20; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_K:
+            twist = Twist()
+            twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_L:
+            twist = Twist()
+            twist.linear.x = 20; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_M:
+            twist = Twist()
+            twist.linear.x = -20; twist.linear.y = -20; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_Comma:
+            twist = Twist()
+            twist.linear.x = 0; twist.linear.y = -20; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_Period:
+            twist = Twist()
+            twist.linear.x = 20; twist.linear.y = -20; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0 
+        if twist != None:
+            self.cmdVelPub.publish(twist)
+
+    def cmdVelCallback(self, msg):
+        cmd_vel_text = "x=" + str(msg.linear.x) + " y=" + str(msg.linear.y)
+        self.emit(QtCore.SIGNAL("sendCmdVelText(PyQt_PyObject)"), cmd_vel_text) 
+
+    def updateCmdVelTextbox(self, value):
+        self.cmdVelTextbox.moveCursor(QtGui.QTextCursor.End)
+        self.cmdVelTextbox.ensureCursorVisible()
+        self.cmdVelTextbox.append(str(value))
+        self.cmdVelTextbox.update()
+        
+
+if __name__ == '__main__':
+
+    app = QtGui.QApplication(sys.argv)
+    w = SpheroIntrudeForm()
+    w.show()
+    sys.exit(app.exec_())
+  
+        
