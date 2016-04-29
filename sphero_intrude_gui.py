4134b11e1b9bd352dd24404f7ab15be0ff57c2c9
= <eric_romrell@byu.edu>
2016-04-29 16:56:20 -0600

    Added sphero file

---
+++ sphero_intrude_gui.py
@@ -0,0 +1,347 @@
+#!/usr/bin/python
+
+import sys, rospy, math
+from PyQt4 import QtGui, QtCore
+from geometry_msgs.msg import Twist, Pose2D
+from std_msgs.msg import ColorRGBA, Float32, Bool
+from apriltags_intrude_detector.srv import apriltags_intrude
+from apriltags_intrude_detector.srv import apriltags_info
+
+# Point Class
+# Created by Jon Francis
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+# Vector Class
+# Created by Jon Francis
+class Vector:
+
+    def __init__(self, direction, magnitude):
+        self.direction = direction
+        self.magnitude = magnitude
+
+    def getPoint(self):
+	point = Point()
+	point.x = self.magnitude * math.acos(self.direction)
+	ponit.y = self.magnitude * math.asin(self.direction)
+	return point
+
+# Field Class
+# Created by Jon Francis
+class Field:
+
+    def __init__(self, id):
+        self.id = id
+
+    def getVect(self, point):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+
+# PolyField Class
+# Created by Jon Francis
+class PolyField(Field):
+
+    def __init__(self, id, poly):
+        #super(self.__class__, self).__init__(id)
+	Field.__init__(self, id)
+        self.poly = poly
+
+    def addPoint(self, x, y):
+	self.poly.append(Point(x, y))
+
+    def getCenter(self):
+        count = x = y = 0
+        for p in self.poly:
+            count += 1
+            x += int(p.x)
+            y += int(p.y)
+        x /= count
+        y /= count
+        return Point(x, y)
+
+    def getDistanceFromCenter(self, point):
+        c = self.getCenter()
+        x = c.x - point.x
+        y = c.y - point.y
+        x *= x
+        y *= y
+        return math.sqrt(x + y)
+
+    def getAngleToCenter(self, point):
+        c = self.getCenter()
+        x = c.x - point.x
+        y = c.y - point.y
+        return math.atan2(y, x)
+
+    def getCircleRadius(self):
+        return self.getDistanceFromCenter(self.poly[0])
+
+    def inCircle(self, point):
+        if self.getDistanceFromCenter(point) < self.getCircleRadius():
+            return true
+        else:
+            return false
+
+    def inBound(self, point, dist):
+        if self.inCircle(point):
+            return false
+        elif self.getDistanceFromCenter(point) < self.getCircleRadius() + dist:
+            return true
+        else:
+            return false
+
+
+# AttractiveField Class
+# Created by Jon Francis
+class AttractiveField(PolyField):
+
+    def __init__(self, id, alpha, bound):
+        #super(self.__class__, self).__init__(id)
+	poly
+
+	PolyField.__init__(self, id, poly)
+        self.alpha = alpha
+        self.bound = bound
+
+    def getVect(self, point):
+        if self.inCircle(point):
+            return Vector(0, 0)
+        #elif self.inBound(point, bound):
+        #    return Vector(self.getAngleToCenter(point), self
+	else:
+	    return Vector(0, self.alpha)
+
+# You implement this class
+class Controller:
+    stop = True # This is set to true when the stop button is pressed
+
+    def __init__(self):
+        self.cmdVelPub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.trackposSub = rospy.Subscriber("tracked_pos", Pose2D, self.trackposCallback)
+
+    def trackposCallback(self, msg):
+        # This function is continuously called
+        if not self.stop:
+            twist = Twist()
+            # Change twist.linear.x to be your desired x velocity
+	    #velocity = self.getVelocity(msg)
+	    print polyPoints
+            twist.linear.x = 0
+            # Change twist.linear.y to be your desired y velocity
+            twist.linear.y = 0
+            twist.linear.z = 0
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = 0
+            self.cmdVelPub.publish(twist)
+
+    def getVelocity(self, msg):
+	spheroPoint = Point(msg.x, msg.y)
+	goal = AttractiveField(0, 80, 0)
+	vector = goal.getVect(spheroPoint)
+	return vector.getPoint()
+
+    def start(self):
+        rospy.wait_for_service("apriltags_info")
+        try:
+            info_query = rospy.ServiceProxy("apriltags_info", apriltags_info)
+            resp = info_query()
+
+            for i in range(len(resp.polygons)):
+                # A polygon (access points using poly.points)
+                poly = resp.polygons[i]
+                # The polygon's id (just an integer, 0 is goal, all else is bad)
+                t_id = resp.ids[i]
+		field = PolyField(t_id)
+		for point in poly.points:
+		    field.addPoint(point.x, point.y)
+		self.polyFields.append(field)
+
+        except Exception, e:
+            print "Exception: " + str(e)
+        finally:
+            self.stop = False
+
+    def stop(self):
+        self.stop = True
+
+
+class SpheroIntrudeForm(QtGui.QWidget):
+    controller = Controller()
+    
+    def __init__(self):
+        super(QtGui.QWidget, self).__init__()
+        self.resize(600, 480) 
+        self.initUI()
+
+        rospy.init_node('sphero_intrude', anonymous=True)
+        self.cmdVelPub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.cmdVelSub = rospy.Subscriber("cmd_vel", Twist, self.cmdVelCallback) 
+
+        self.trackposSub = rospy.Subscriber("tracked_pos", Pose2D, self.trackposCallback) 
+       
+    def initUI(self):
+
+        self.stateLabel = QtGui.QLabel("Position")
+        self.stateTextbox = QtGui.QTextEdit()
+        self.stateTextbox.setReadOnly(True)
+        self.connect(self, QtCore.SIGNAL("sendPosIDText(PyQt_PyObject)"), self.updateStateTextbot)     
+        
+        key_instruct_label = """
+	Control Your Sphero!
+	---------------------------
+	Moving around:
+	   u    i    o
+	   j    k    l
+	   m    ,    .
+	"""
+        self.keyInstructLabel = QtGui.QLabel(key_instruct_label)
+        self.cmdVelLabel = QtGui.QLabel("cmd_vel")
+        self.cmdVelTextbox = QtGui.QTextEdit()
+        self.cmdVelTextbox.setReadOnly(True)  
+        self.connect(self, QtCore.SIGNAL("sendCmdVelText(PyQt_PyObject)"), self.updateCmdVelTextbox)
+
+        self.aprilTagsInfoLabel = QtGui.QLabel("april tags info")
+        self.aprilTagsInfoBtn = QtGui.QPushButton("Query")
+        self.aprilTagsInfoBtn.clicked.connect(self.queryAprilTagsInfo)
+        self.aprilTagsTextbox = QtGui.QTextEdit()
+        self.aprilTagsTextbox.setReadOnly(True)
+        self.connect(self, QtCore.SIGNAL("sendTagInfoText(PyQt_PyObject)"), self.updateAprilTagsTextbot)
+
+        self.aprilTagsStartBtn = QtGui.QPushButton("Start")
+        self.aprilTagsStartBtn.clicked.connect(self.controller.start)
+
+        self.aprilTagsStopBtn = QtGui.QPushButton("Stop")
+        self.aprilTagsStopBtn.clicked.connect(self.controller.stop)
+
+
+        self.layout =  QtGui.QVBoxLayout()
+        self.layout.addWidget(self.stateLabel)
+        self.layout.addWidget(self.stateTextbox)
+        self.layout.addWidget(self.keyInstructLabel)
+        self.layout.addWidget(self.cmdVelLabel)
+        self.layout.addWidget(self.cmdVelTextbox)
+        hlayout = QtGui.QHBoxLayout()
+        hlayout.addWidget(self.aprilTagsInfoLabel)
+        hlayout.addWidget(self.aprilTagsInfoBtn)
+        hlayout.addWidget(self.aprilTagsStartBtn)
+        hlayout.addWidget(self.aprilTagsStopBtn)
+        self.layout.addLayout(hlayout)
+        self.layout.addWidget(self.aprilTagsTextbox)
+        self.setLayout(self.layout)
+
+        self.setWindowTitle("Sphero Intrude")
+        self.show()
+
+    def keyPressEvent(self, e): 
+        twist = None       
+        if e.key() == QtCore.Qt.Key_U:
+            twist = Twist()
+            twist.linear.x = -80; twist.linear.y = 80; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_I:
+            twist = Twist()  
+            twist.linear.x = 0; twist.linear.y = 80; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0     
+        elif e.key() == QtCore.Qt.Key_O:
+            twist = Twist()
+            twist.linear.x = 80; twist.linear.y = 80; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_J:
+            twist = Twist()
+            twist.linear.x = -80; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_K:
+            twist = Twist()
+            twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_L:
+            twist = Twist()
+            twist.linear.x = 80; twist.linear.y = 0; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_M:
+            twist = Twist()
+            twist.linear.x = -80; twist.linear.y = -80; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_Comma:
+            twist = Twist()
+            twist.linear.x = 0; twist.linear.y = -80; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        elif e.key() == QtCore.Qt.Key_Period:
+            twist = Twist()
+            twist.linear.x = 80; twist.linear.y = -80; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0 
+        if twist != None:
+            self.cmdVelPub.publish(twist)
+
+    def cmdVelCallback(self, msg):
+        cmd_vel_text = "x=" + str(msg.linear.x) + " y=" + str(msg.linear.y)
+        self.emit(QtCore.SIGNAL("sendCmdVelText(PyQt_PyObject)"), cmd_vel_text) 
+
+    def updateCmdVelTextbox(self, value):
+        self.cmdVelTextbox.moveCursor(QtGui.QTextCursor.End)
+        self.cmdVelTextbox.ensureCursorVisible()
+        self.cmdVelTextbox.append(str(value))
+        self.cmdVelTextbox.update()
+
+    def trackposCallback(self, msg):
+        rospy.wait_for_service("apriltags_intrude")
+        try:
+            intrude_query = rospy.ServiceProxy("apriltags_intrude", apriltags_intrude)
+            resp = intrude_query(int(msg.x), int(msg.y))
+            pos_id_text = "["+str(int(msg.x))+"," +str(int(msg.y))+"]" + "(" + str(resp.id) + ")"
+            self.emit(QtCore.SIGNAL("sendPosIDText(PyQt_PyObject)"), pos_id_text)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
+    def updateStateTextbot(self, value):
+        self.stateTextbox.moveCursor(QtGui.QTextCursor.End)
+        self.stateTextbox.ensureCursorVisible()
+        self.stateTextbox.append(str(value))
+        self.stateTextbox.update()
+
+    def queryAprilTagsInfo(self):
+        #print "clicked"
+        rospy.wait_for_service("apriltags_info")
+        try:
+            info_query = rospy.ServiceProxy("apriltags_info", apriltags_info)
+            resp = info_query()
+               
+            #print str(resp)
+
+            info_text = "" 
+            for i in range(len(resp.polygons)):
+                poly = resp.polygons[i]
+                t_id = resp.ids[i]
+
+                #print(str(poly))
+                #print(str(t_id))
+                info_text += "["+str(t_id)+"] "
+                for p in poly.points:
+                    info_text += "(" + str(int(p.x)) + "," + str(int(p.y)) + ")"
+                info_text += "\n" 
+
+            self.emit(QtCore.SIGNAL("sendTagInfoText(PyQt_PyObject)"), info_text)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+        
+    def updateAprilTagsTextbot(self, value):
+        self.aprilTagsTextbox.clear()
+        self.aprilTagsTextbox.moveCursor(QtGui.QTextCursor.End)
+        self.aprilTagsTextbox.ensureCursorVisible()
+        self.aprilTagsTextbox.append(str(value))
+        self.aprilTagsTextbox.update()        
+
+
+if __name__ == '__main__':
+
+    app = QtGui.QApplication(sys.argv)
+    w = SpheroIntrudeForm()
+    w.show()
+    sys.exit(app.exec_())
+  
+        
