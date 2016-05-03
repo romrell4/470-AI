4e8f80ff3dcd6d767118ef83c42e9ba62ea51a02
Eric Romrell <eric_romrell@byu.edu>
2016-05-02 21:21:17 -0600

    Added beginnings to discretizing area

--- lab2.py
+++ lab2.py
@@ -1,2 +1,39 @@
 #!/bin/bash
-print "Hello World"
+
+SIZE_OF_GRID = 100
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def inAprilTag(self):
+    	#TODO: Fill in logic
+    	return False
+
+	def __str__(self):
+		return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+def discretizeArea():
+	lowerRightPoint = getLowerRightPoint()
+	gridCenters = getGridCenters(lowerRightPoint)
+	getGridMap(gridCenters)
+
+def getLowerRightPoint():
+	#TODO: Fill in logic
+	return Point(1500, 1400)
+
+def getGridCenters(maxPoint):
+	gridCenters = []
+	for x in range(SIZE_OF_GRID/2, maxPoint.x, SIZE_OF_GRID):
+		for y in range(SIZE_OF_GRID/2, maxPoint.y, SIZE_OF_GRID):
+			point = Point(x, y)
+			if !point.inAprilTag():
+				gridCenters.append(point)
+	return gridCenters
+
+def getGridMap(gridCenters):
+
+
+discretizeArea()
+
