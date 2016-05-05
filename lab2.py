b63aa1090078d2325268c1c53637ce58ed207ebc
Eric Romrell <eric_romrell@byu.edu>
2016-05-05 15:05:57 -0600

    Implemented getRandomPoint

--- lab2.py
+++ lab2.py
@@ -1,5 +1,6 @@
 #!/bin/bash
 import math
+import random
 
 SIZE_OF_GRID = 10
 # SIZE_OF_GRID = 10
@@ -279,6 +280,10 @@ class RRT:
 
     def getRandPoint(self):
         #Return a new random point within the boundaries of the world
+        bounds = getLowerRightPoint();
+        x = random.random() * bounds.x;
+        y = random.random() * bounds.y;
+        return Point(x, y)
 
     def getNearPoint(self, randPoint, treeSoFar):
         #Return the point in treeSoFar that is closest to randPoint
