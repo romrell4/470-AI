7c260e3d4cf1c14f554f1518914c8610426abdc8
Eric Romrell <eric_romrell@byu.edu>
2016-05-12 14:21:18 -0600

    Added comments

--- Reversi/Reversi.py
+++ Reversi/Reversi.py
@@ -12,14 +12,34 @@ class Reversi:
 		# Print the board to start
 		print self.board
 
+		done = False
+
 		while(True):
+			#Check for all posssible locations
 			possibilities = self.board.getPlayableSquares(self.turn)
+
+			#Check if you had no possible locations
 			if len(possibilities) == 0:
+				#Check if the other player also couldn't play. If so, the game is over
+				if done:
+					return
+
+				#Set the variable to notify the program that you couldn't go
+				done = True
 				self.endTurn()
 				continue
-			choice = possibilities[0]
+
+			#Set the variable to notify the program that you could go	
+			done = False
+
+			#Get the best choice and play it
+			choice = self.getChoice(possibilities)
 			self.board.play(choice.x, choice.y, self.turn)
 			self.endTurn()
+
+	def getChoice(self, possibilities):
+		#TODO: Add logic to choose the best possibility
+		return possibilities[0]
 
 	def endTurn(self):
 		print self.board
