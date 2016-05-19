64596f62e2635492f9f39d1d2f2fd4c13d3da7b0
Eric Romrell <eric_romrell@byu.edu>
2016-05-19 08:23:01 -0600

    Added timer and optimized slightly

--- PartOfSpeech/parser.py
+++ PartOfSpeech/parser.py
@@ -1,3 +1,7 @@
+import time
+
+start = time.time()
+
 fileName = "trainingData.txt"
 # fileName = "test.txt"
 
@@ -9,13 +13,18 @@ for mapping in data.split():
 	parts = mapping.split("_")
 
 	key = parts[1]
-	value = parts[0]
+	value = parts[0].lower()
 
 	if key not in partsOfSpeech:
 		partsOfSpeech[key] = {}
 
 	if value not in partsOfSpeech[key]:
-		partsOfSpeech[key][value] = 1
-	else:
-		partsOfSpeech[key][value] += 1
-print partsOfSpeech
+		partsOfSpeech[key][value] = 0
+	partsOfSpeech[key][value] += 1
+
+for partOfSpeech in partsOfSpeech:
+	print partOfSpeech
+
+
+end = time.time()
+print(end - start)
